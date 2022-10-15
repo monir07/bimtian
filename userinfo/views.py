b89ea22860b6608c3d2ba4376347428a84672a5c
monir07 <monirulslm7@gmail.com>
2022-10-15 15:45:11 +0600

    install crispy form

--- userinfo/views.py
+++ userinfo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views import View
-
+from django.views import View, generic
+from .form import *
+from django.contrib.auth.models import User
+from django.db import transaction
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 # Create your views here.
 class CustomPermissionMixin(PermissionRequiredMixin):
@@ -29,7 +33,13 @@ class CommonMixin(SuccessMessageMixin, LoginRequiredMixin, CustomPermissionMixin
         return data
 
 
-class LoginView(View):
+class LoginView(SuccessMessageMixin, generic.CreateView):
+    model = User
+    form_class = NewUserForm
+    template_name = 'indent/material_demand/form.html'
+    success_message = "Information added successfully"
+    title = "Add Material Demand"
+    # success_url = reverse_lazy("material_demand_list")
     template_name = 'authentication/login.html'
 
     def get(self, request, *args, **kwargs):
@@ -39,11 +49,21 @@ class LoginView(View):
         return render(request, self.template_name, context)
 
 
-class SingupView(View):
+class SingupView(generic.CreateView):
+    model = User
+    form_class = NewUserForm
+    success_message = "User created successfully"
+    title = "New user signup"
     template_name = 'authentication/signup.html'
+    uccess_url = reverse_lazy("login")
 
-    def get(self, request, *args, **kwargs):
-        context = {
-            
-        }
-        return render(request, self.template_name, context)
+    def form_valid(self, form, *args, **kwargs):
+        with transaction.atomic():
+            self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_context_data(self, **kwargs):
+        form = self.get_form()
+        kwargs['form'] = form
+        kwargs['title'] = self.title
+        return super().get_context_data(**kwargs)
